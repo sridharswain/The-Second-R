0219187aba3ac1fcf74cf228222667bbc21cd6b5
sridharswain <sridharswain25@gmail.com>
2018-08-08 19:21:24 +0530

    Firebase Admin SDK Added

--- src/index.js
+++ src/index.js
@@ -7,12 +7,6 @@ const db = firebase.init();
 const app = express();
 routes(app,db);
 
-app.get("/",(req,res)=>{
-    const ref = db.ref('test');
-    ref.child('a').set("Hll");
-    res.send("Hello Worlds");
-});
-
 app.listen(8080,()=>{
     console.log("App Started on port 8080");
 })
