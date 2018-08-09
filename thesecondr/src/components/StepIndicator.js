29819338c8b97cbf4469c5e15785581f6b57c679
sridharswain <sridharswain25@gmail.com>
2018-08-10 00:13:17 +0530

    New Line at EOF Added

--- thesecondr/src/components/StepIndicator.js
+++ thesecondr/src/components/StepIndicator.js
@@ -12,31 +12,27 @@ export default class StepIndicator extends Component{
 
   componentWillMount(){
     var window= Dimensions.get('window');
-    this.setState({width:window.width*0.85});
+    this.setState({width:window.width*0.80});
   }
 
   render(){
     let personal = require('../res/images/personal.png');
     let finger = require('../res/images/id.png');
-    let pass = require('../res/images/locked_grey.png');
-    let camera = require('../res/images/camera.png');
+    let pass = require('../res/images/key.png');
+    let address = require('../res/images/map.png');
     return(
         <View style={[this.props.style,{alignItems:'center',justifyContent :'center'}]}>
-            <Bar borderWidth={2} progress={(this.props.current-1)*0.33} width={this.state.width} borderColor={'black'} color={'black'}/>
+            <Bar borderWidth={1.5} progress={(this.props.current-1)/2} width={this.state.width} borderColor={'black'} color={'black'}/>
             <View style={[styles.stepStyle,{left :0,backgroundColor : (this.props.current>=1?'black':'white')}]}>
                 <Image style={[styles.innerStyle,{tintColor :(this.props.current>=1?'white':'#878787') }]} source={personal}/>
             </View>
 
-            <View style={[styles.stepStyle,{left :((this.state.width)/3.33),backgroundColor : (this.props.current>=2?'black':'white')}]}>
-                <Image style={[styles.innerStyle,{tintColor :(this.props.current>=2?'white':'#878787') }]} source={finger}/>
+            <View style={[styles.stepStyle,{right :((this.state.width)/2.22),backgroundColor : (this.props.current>=2?'black':'white')}]}>
+                <Image style={[styles.innerStyle,{tintColor :(this.props.current>=2?'white':'#878787') }]} source={pass}/>
             </View>
 
-            <View style={[styles.stepStyle,{right :((this.state.width)/3.33),backgroundColor : (this.props.current>=3?'black':'white')}]}>
-                <Image style={[styles.innerStyle,{tintColor :(this.props.current>=3?'white':'#878787') }]} source={pass}/>
-            </View>
-
-            <View style={[styles.stepStyle,{right :0,backgroundColor : (this.props.current>=4?'black':'white')}]}>
-                <Image style={[styles.innerStyle,{tintColor :(this.props.current>=4?'white':'#878787') }]} source={camera}/>
+            <View style={[styles.stepStyle,{right :0,backgroundColor : (this.props.current>=3?'black':'white')}]}>
+                <Image style={[styles.innerStyle,{tintColor :(this.props.current>=3?'white':'#878787') }]} source={address}/>
             </View>
         </View>
     );
