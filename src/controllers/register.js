ca0b859a5c329fabfd705a4332907f6b1833a521
sridharswain <sridharswain25@gmail.com>
2018-08-27 22:40:46 +0530

    User Registration Completed

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,8 +1,16 @@
-module.exports = function(data,db){
-    console.log(data);
-    const userRef = db.ref('users/');
-    userRef.child(data.user).once('value',function(snapshot){
-        console.log(snapshot.exists());
-        console.log(snapshot.val());
+var User = require("../models/user")
+
+module.exports = function(data,db,callback){
+    var user = new User({
+        name : data.name,
+        email : data.email,
+        phone : data.phone,
+        address : data.address,
+        password : data.password
+    });
+
+    user.save(function(err){
+        if(err) callback("User Already Exists");
+        else callback()
     });
 }
