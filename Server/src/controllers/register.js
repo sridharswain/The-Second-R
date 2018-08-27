b7af3c31db009345ac1cb2c97bc98eea8962ad7e
sridharswain <sridharswain25@gmail.com>
2018-08-27 20:45:23 +0530

    Mongo DB Added and Confrigured

--- Server/src/controllers/register.js
+++ Server/src/controllers/register.js
@@ -1,3 +1,8 @@
 module.exports = function(data,db){
-    
+    console.log(data);
+    const userRef = db.ref('users/');
+    userRef.child(data.user).once('value',function(snapshot){
+        console.log(snapshot.exists());
+        console.log(snapshot.val());
+    });
 }
