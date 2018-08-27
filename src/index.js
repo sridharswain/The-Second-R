a0cf8f11ec6e7fbe32a8adb5469497d54199aefc
sridharswain <sridharswain25@gmail.com>
2018-08-27 20:45:23 +0530

    Mongo DB Added and Confrigured

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 'use strict'
 const express = require("express");
 const routes = require("./routes");
-const firebase = require("./firebaseinit");
-const db = firebase.init();
+const firebase = require("./firebase");
+//const db = firebase.init();
+const db = require("mongoose");
+db.connect("mongodb://localhost/thesecondr",function(err){
+    if(err) throw err;
+    else console.log("Connected to DB");  
+})
 
 const app = express();
 app.use(express.json());
 routes(app,db);
 
-app.listen(8080,()=>{
+app.listen(8080,() => {
     console.log("App Started on port 8080");
 })
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
+const register = require('./controllers/register');
+
 module.exports = function(app,db){
     app.post("/register",(req,res)=>{
-        console.log(req.body.user);
+        console.log(req.body);
+        register(req.body,db);
         res.send("Hello World");
     });
 }
