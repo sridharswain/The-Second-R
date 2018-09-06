d9850ed505b0800729e3f779c7ca62e94043d613
sridharswain <sridharswain25@gmail.com>
2018-09-06 19:12:02 +0530

    Ad Cards complete

--- thesecondr/src/screens/browse.js
+++ thesecondr/src/screens/browse.js
@@ -1,19 +1,63 @@
 import React,{Component} from 'react';
-import {View,Text,Image} from 'react-native';
+import {View,Text,Image,FlatList,StyleSheet} from 'react-native';
+import { get } from '../utils/request';
 import Styles from '../res/styles';
 
 export default class Browse extends Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            orders : []
+        }
+    }
+
+    componentDidMount(){
+        this.getAllAds();
+    }
+
+    getAllAds = () => {
+        get('/getAds',{})
+        .then((response) => {
+            this.setState({orders : response.res});
+        });
+    }
+
+    renderItem = (item) => {
+        return(
+            <View style={styles.cardRoot}>
+                <Image source={{uri : item.imageLink[0]}} style={{height : 250, width : '100%'}}/>
+                <Text style={styles.cardTitleStyle}>{item.title}</Text>
+                <Text style={[styles.cardTitleStyle,{fontSize:16}]}>{"Rs. "+item.cost}</Text>
+            </View>
+        );
     }
 
     render(){
         return(
             <View style={Styles.container}>
-                <Image source={require('../res/images/route.png') }/>
-                <Text style ={{color : 'blue'}}>Browse</Text>
+                <FlatList data={this.state.orders}
+                    renderItem={({ item }) => this.renderItem(item)}
+                    keyExtractor = {item => item._id} />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    cardRoot : {
+        alignItems : 'center',
+        margin : 3,
+        padding : 10,
+        backgroundColor : 'white',
+        elevation : 4,
+        borderRadius : 2
+    },
+    cardTitleStyle :{
+        alignSelf : 'flex-start',
+        fontSize : 20,
+        fontWeight : 'bold',
+        color : 'black',
+        marginVertical : 2
+    }
+});
